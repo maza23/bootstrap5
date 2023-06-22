06aabc045d179a813309afb8935e891d5b7fbded
maza23 <eltapiumaza@gmail.com>
2023-06-22 10:58:17 -0300

    1 binaries uploaded

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//
+// Place any custom JS here
+//
+import * as bootstrap from 'bootstrap';
+
+document.querySelectorAll('[data-bs-toggle="popover"]').forEach((popover) => {
+  new bootstrap.Popover(popover);
+});
